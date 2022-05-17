2b8e77e1c6b99028dbbc892d7fdfabb64c1eda8a
Halloerik <halloerik95@gmail.com>
2022-05-17 18:08:01 +0200

    Fixed use of custom emojis

--- bot.py
+++ bot.py
@@ -3,14 +3,15 @@ Created on 01.05.2020
 
 @author: Erik
 """
+import pickle
 import sys
 import traceback
 
 import discord
 import discord.ext.commands as commands
-import pickle
+from discord.errors import Forbidden, LoginFailure, InvalidArgument
 from discord.ext.commands import has_permissions, MissingPermissions, RoleNotFound
-from discord.errors import Forbidden, LoginFailure
+from emoji import UNICODE_EMOJI
 
 bot = commands.Bot('$')
 
@@ -29,13 +30,12 @@ async def hello(ctx):
     await ctx.channel.send('Hello!')
 
 
-'''
-@bot.command(name="logout", pass_context=True)
+'''@bot.command(name="logout", pass_context=True)
 @has_permissions(administrator=True)
 async def logout(ctx):
     """Shuts the bot down"""
     await ctx.channel.send('Good bye!')
-    await bot.logout()
+    await bot.close()
 
 
 @logout.error
@@ -44,7 +44,19 @@ async def logout_error(ctx, error):
         await ctx.channel.send("You aren't an admin. Cringe :sick:")
     else:
         traceback.print_exc()
-'''
+
+@bot.command(name="purge", pass_context=True)
+@has_permissions(administrator=True)
+async def purge(ctx):
+    """Shuts the bot down"""
+    await ctx.channel.purge()
+
+@purge.error
+async def purge_error(ctx, error):
+    if isinstance(error, MissingPermissions):
+        await ctx.channel.send("You aren't an admin. Cringe :sick:")
+    else:
+        traceback.print_exc()'''
 
 
 @bot.command(name="listen", pass_context=True)
@@ -113,6 +125,7 @@ async def add_role(ctx: commands.context.Context):
             role = await commands.RoleConverter().convert(ctx, command_parts[2])
             try:
                 emoji = await commands.PartialEmojiConverter().convert(ctx, command_parts[3])
+                emoji = emoji.id
             except commands.PartialEmojiConversionFailure:
                 emoji = command_parts[3]
 
@@ -126,18 +139,34 @@ async def add_role(ctx: commands.context.Context):
                 await ctx.send("This emoji is already used!")
                 return
 
-            role_emoji_list.append({"game": game, "role": role, "emoji": emoji})
+            new_emoji = {"game": game, "role": role, "emoji": emoji}
+            role_emoji_list.append(new_emoji)
             try:
+                save_comments()
                 text = "Click an Emote to get notified when someone wants to play the corresponding game\n\n"
                 for d in role_emoji_list:
-                    text += f"{d['emoji']} → {d['game']}\n"
-                    await role_comment.add_reaction(d['emoji'])
+                    emoji_ = await get_emoji(ctx.guild, d['emoji'])
+                    text += f"{emoji_} → {d['game']}\n"
+                    await role_comment.add_reaction(emoji_)
                 await role_comment.edit(content=text)
-                save_comments()
 
             except discord.NotFound:
                 await ctx.channel.send("The Comment I was listening too was deleted. I'm going to stop listening here.")
                 await stop_listening_for_roles(ctx)
+            except TypeError:
+                print(game)
+                print(role)
+                print(emoji)
+                traceback.print_exc()
+                role_emoji_list.remove(new_emoji)
+                save_comments()
+            except InvalidArgument:
+                print(game)
+                print(role)
+                print(emoji)
+                traceback.print_exc()
+                role_emoji_list.remove(new_emoji)
+                save_comments()
             return
 
     await ctx.channel.send("You are using this command wrong. $add_role roleName rolePing emoji\n\
@@ -178,22 +207,26 @@ async def remove_role(ctx: commands.context.Context):
                 role = None
             try:
                 emoji = await commands.PartialEmojiConverter().convert(ctx, command_parts[1])
+                emoji = emoji.id
             except commands.PartialEmojiConversionFailure:
                 emoji = command_parts[1]
 
+            # print(f"{game}, {role}, {emoji}")
+            # print(role_emoji_list)
             emoji_role = [emoji_role for emoji_role in role_emoji_list if (game in emoji_role.values()
                                                                            or role in emoji_role.values()
                                                                            or emoji in emoji_role.values())]
             if emoji_role:
                 try:
                     role_emoji_list.remove(emoji_role[0])
-                    await role_comment.clear_reaction(emoji_role[0]['emoji'])
+                    await role_comment.clear_reaction(await get_emoji(ctx.guild, emoji_role[0]['emoji']))
                     save_comments()
                     if role_emoji_list:  # if still some emojis left
                         text = "Click an Emote to get notified when someone wants to play the corresponding game\n\n"
                         for d in role_emoji_list:
-                            text += f"{d['emoji']} → {d['game']}\n"
-                            await role_comment.add_reaction(d['emoji'])
+                            emoji = await get_emoji(ctx.guild, d['emoji'])
+                            text += f"{emoji} → {d['game']}\n"
+                            await role_comment.add_reaction(emoji)
                         await role_comment.edit(content=text)
                     else:
                         await role_comment.edit(content="No roles left."
@@ -245,11 +278,7 @@ async def on_raw_reaction_add(reaction_event: discord.RawReactionActionEvent):
             return
         for d in role_emoji_list:
             # print(type(reaction_event.emoji), reaction_event.emoji)
-            if isinstance(d["emoji"], str):
-                if reaction_event.emoji.name == d["emoji"]:
-                    await user.add_roles(d["role"])
-                    return
-            elif reaction_event.emoji == d["emoji"]:
+            if reaction_event.emoji.name == d["emoji"] or reaction_event.emoji.id == d["emoji"]:
                 await user.add_roles(d["role"])
                 return
         await role_comment.remove_reaction(reaction_event.emoji, user)  # remove an untracked emoji
@@ -271,14 +300,9 @@ async def on_raw_reaction_remove(reaction_event: discord.RawReactionActionEvent)
         if user == bot.user:
             # print("i reacted myself")
             return
-
         for d in role_emoji_list:
             # print(type(reaction_event.emoji), reaction_event.emoji)
-            if isinstance(d["emoji"], str):
-                if reaction_event.emoji.name == d["emoji"]:
-                    await user.remove_roles(d["role"])
-                    return
-            elif reaction_event.emoji == d["emoji"]:
+            if reaction_event.emoji.name == d["emoji"] or reaction_event.emoji.id == d["emoji"]:
                 await user.remove_roles(d["role"])
                 return
 
@@ -307,18 +331,17 @@ Other than that feel free and welcome to express yourself.''')
 
 
 def save_comments():
-    #print(role_comments)
-    #print(role_emojis)
+    # print(role_comments)
+    # print(role_emojis)
 
     cleaned_role_comments = {guild_id: (message.channel.id, message.id) for guild_id, message in role_comments.items()}
-    cleaned_role_emojis = {guild_id:
-        [
-            {"game": emoji_dict["game"], "role": emoji_dict["role"].id, "emoji": emoji_dict["emoji"]}
-            for emoji_dict in emoji_list
-        ]
+    cleaned_role_emojis = {guild_id: [
+        {"game": emoji_dict["game"], "role": emoji_dict["role"].id, "emoji": emoji_dict["emoji"]}
+        for emoji_dict in emoji_list
+    ]
         for guild_id, emoji_list in role_emojis.items()}
-    #print(cleaned_role_comments)
-    #print(cleaned_role_emojis)
+    # print(cleaned_role_comments)
+    # print(cleaned_role_emojis)
     pickle.dump((cleaned_role_comments, cleaned_role_emojis), open("role_comments", "wb"))
 
 
@@ -327,23 +350,32 @@ async def load_comments():
     global role_emojis
     try:
         role_comments, role_emojis = pickle.load(open("role_comments", "rb"))
-        #print(f"loading {role_comments}")
-        #print(f"loading {role_emojis}")
+        # print(f"loading {role_comments}")
+        # print(f"loading {role_emojis}")
         for guild_id, message_tuple in role_comments.items():
             channel = await bot.fetch_channel(message_tuple[0])
             message = await channel.fetch_message(message_tuple[1])
-            role_comments[guild_id]=message
+            role_comments[guild_id] = message
 
-        for guild_id,emoji_list in role_emojis.items():
+        for guild_id, emoji_list in role_emojis.items():
             guild = await bot.fetch_guild(guild_id)
             for emoji_dict in emoji_list:
                 emoji_dict["role"] = guild.get_role(emoji_dict["role"])
 
-        #print(f"loaded {role_comments}")
-        #print(f"loaded {role_emojis}")
+        # print(f"loaded {role_comments}")
+        # print(f"loaded {role_emojis}")
 
     except FileNotFoundError:
         save_comments()
+
+
+async def get_emoji(guild, emoji):
+    # print(UNICODE_EMOJI['en'].keys())
+    if emoji in UNICODE_EMOJI['en'].keys():
+        return emoji
+    else:
+        emoji = await guild.fetch_emoji(emoji)
+        return emoji
 
 
 if __name__ == '__main__':
